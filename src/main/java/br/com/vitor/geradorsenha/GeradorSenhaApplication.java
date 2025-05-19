eb77391e12673164409f0abe62a1d31592134abd
Vitor Stosick <vitor.stosick@alunos.sc.senac.br>
2025-05-19 10:51:33 -0300

    first commit

---
+++ src/main/java/br/com/vitor/geradorsenha/GeradorSenhaApplication.java
@@ -0,0 +1,13 @@
+package br.com.vitor.geradorsenha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorSenhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorSenhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vitor/geradorsenha/GeradorSenhaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vitor.geradorsenha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorSenhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
